b9187c7da133e07a2e13f0f50a48e4bbde509560
sr1jan <srofficialsingh@gmail.com>
2023-07-17 21:17:52 +0530

    add adapter apis, add utils function

--- routes/adapter.js
+++ routes/adapter.js
@@ -2,15 +2,108 @@ const express = require("express");
 
 const apm = require("../apm");
 
+const {
+  getUserGuilds,
+  getUserGuildMember,
+  refreshAccessToken,
+  // getUserFromDb,
+} = require("../utils/discordApi");
+
 const { authMiddleware } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.get("/userGuilds", authMiddleware, async (req, res, next) => {
-  let { accessToken } = req.query;
+router.get("/isGuildMember", authMiddleware, async (req, res, next) => {
+  const { accessToken, refreshToken, guild_id } = req.query;
+  let newAccessToken,
+    newRefreshToken,
+    member = false;
+
+  console.log("accessToken", accessToken)
+  console.log("refreshToken", refreshToken)
+
   try {
-    console.log("accesstoken", accessToken);
-    res.status(200).json({ accessToken: accessToken });
+    // const user = await getUserFromDb(user_id);
+    // if (!user) {
+    //   return res.status(404).send({ message: "User not found!" });
+    // }
+
+    let userGuildsRes = await getUserGuilds(accessToken, false);
+    if (userGuildsRes.error) {
+      if (userGuildsRes.status == 401) {
+        const refreshRes = await refreshAccessToken(refreshToken);
+        if (refreshRes.error) {
+          return res.status(400).send({ message: refreshRes.error.message });
+        }
+        newAccessToken = refreshRes.data.access_token;
+        newRefreshToken = refreshRes.data.refresh_token;
+        userGuildsRes = await getUserGuilds(newAccessToken, false);
+      } else {
+        return res
+          .status(userGuildsRes.status)
+          .send({ message: userGuildsRes.error.message });
+      }
+    }
+
+    const guilds = userGuildsRes.guilds;
+    const desiredGuild = guilds.find((item) => item.id === guild_id);
+    member = desiredGuild ? true : member;
+    return res.status(200).json({
+      member: member,
+      guild: desiredGuild,
+      newAccessToken: newAccessToken,
+      newRefreshToken: newRefreshToken,
+    });
+  } catch (err) {
+    next(err);
+    apm.captureError(err);
+  }
+});
+
+router.get("/checkRole", authMiddleware, async (req, res, next) => {
+  const { accessToken, refreshToken, guild_id, role_id } = req.query;
+  let newAccessToken,
+    newRefreshToken,
+    role = false;
+
+  console.log("accessToken", accessToken)
+  console.log("refreshToken", refreshToken)
+
+  try {
+    // const user = await getUserFromDb(user_id);
+    // if (!user) {
+    //   return res.status(404).send({ message: "User not found!" });
+    // }
+
+    let userGuildMemberRes = await getUserGuildMember(
+      accessToken,
+      guild_id
+    );
+
+    if (userGuildMemberRes.error) {
+      if (userGuildMemberRes.status == 401) {
+        const refreshRes = await refreshAccessToken(refreshToken);
+        if (refreshRes.error) {
+          return res.status(400).send({ message: refreshRes.error.message });
+        }
+        newAccessToken = refreshRes.data.access_token;
+        newRefreshToken = refreshRes.data.refresh_token;
+        userGuildMemberRes = await getUserGuildMember(newAccessToken, guild_id);
+      } else {
+        return res
+          .status(userGuildMemberRes.status)
+          .send({ message: userGuildMemberRes.error.message });
+      }
+    }
+
+    const member = userGuildMemberRes.member;
+    const role_idx = member.roles?.indexOf(role_id);
+    role = role_idx !== -1 && role_idx !== undefined ? true : role;
+    return res.status(200).json({
+      role: role,
+      newAccessToken: newAccessToken,
+      newRefreshToken: newRefreshToken,
+    });
   } catch (err) {
     next(err);
     apm.captureError(err);
